4540c34c8a5625d00d2afd3db3ccda98aa937842
josip <josipkeresman@gmail.com>
2024-12-25 15:38:02 +0100

    Refactor World impl a bit

--- SimpleGames/Snake/src/World.cpp
+++ SimpleGames/Snake/src/World.cpp
@@ -6,28 +6,41 @@ World::World(const sf::Vector2u &windowSize) : m_blockSize(16), m_windowSize(win
 {
     respwanApple();
 
-    for (size_t i = 0; i < 4; ++i)
+    for (size_t i = 0; i < BOUNDS_SIZE; ++i)
     {
-        m_bounds[i].setFillColor(sf::Color(150, 0, 0));
+        setBoundsColor(i);
+        setBoundsSize(i);
+        setBoundsPosition(i);
+    }
+}
 
-        if (!(i + 1) % 2)
-        {
-            m_bounds[i].setSize(sf::Vector2f(m_windowSize.x, m_blockSize));
-        }
-        else
-        {
-            m_bounds[i].setSize(sf::Vector2f(m_blockSize, m_windowSize.x));
-        }
+void World::setBoundsColor(size_t position)
+{
+    m_bounds[position].setFillColor(sf::Color(150, 0, 0));
+}
 
-        if (i < 2)
-        {
-            m_bounds[i].setPosition(0, 0);
-        }
-        else
-        {
-            m_bounds[i].setOrigin(m_bounds[i].getSize());
-            m_bounds[i].setPosition(sf::Vector2f(m_windowSize));
-        }
+void World::setBoundsSize(size_t position)
+{
+    if (!(position + 1) % 2)
+    {
+        m_bounds[position].setSize(sf::Vector2f(m_windowSize.x, m_blockSize));
+    }
+    else
+    {
+        m_bounds[position].setSize(sf::Vector2f(m_blockSize, m_windowSize.x));
+    }
+}
+
+void World::setBoundsPosition(size_t position)
+{
+    if (position < 2)
+    {
+        m_bounds[position].setPosition(0, 0);
+    }
+    else
+    {
+        m_bounds[position].setOrigin(m_bounds[position].getSize());
+        m_bounds[position].setPosition(sf::Vector2f(m_windowSize));
     }
 }
 
@@ -77,7 +90,7 @@ bool World::isPlayerOutsideTheWorld(const Snake &snake)
 
 void World::render(sf::RenderWindow &window)
 {
-    for (size_t i = 0; i < 4; ++i)
+    for (size_t i = 0; i < BOUNDS_SIZE; ++i)
     {
         window.draw(m_bounds[i]);
     }
--- SimpleGames/Snake/src/headers/World.h
+++ SimpleGames/Snake/src/headers/World.h
@@ -24,11 +24,17 @@ class World
   private:
     bool isPlayerOutsideTheWorld(const Snake &snake);
 
+    void setBoundsSize(size_t position);
+    void setBoundsColor(size_t position);
+    void setBoundsPosition(size_t position);
+
   private:
+    static constexpr uint32_t BOUNDS_SIZE = 4;
+
     sf::Vector2f m_windowSize;
     sf::Vector2i m_appleCoordinates;
     Apple m_apple;
-    sf::RectangleShape m_bounds[4];
+    sf::RectangleShape m_bounds[BOUNDS_SIZE];
     uint32_t m_blockSize;
 };
 } // namespace snake
